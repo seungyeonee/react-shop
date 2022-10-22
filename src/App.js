3597bb1f5bdd0d248288d92db20d81e14466695d
seungyeoni.dev@gmail.com <seungyeoni.dev@gmail.com>
2022-10-22 13:54:47 +0900

    부트스트랩 연결 및 nav바 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import {Navbar, Container, Nav} from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">다사와</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">카테고리1</Nav.Link>
+            <Nav.Link href="#features">카테고리2</Nav.Link>
+            <Nav.Link href="#pricing">카테고리3</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
